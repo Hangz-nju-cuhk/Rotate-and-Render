1fe9db1424b224266637a01aec987672ce583397
Hang_Zhou <zhouhang@link.cuhk.edu.hk>
2020-03-18 15:09:00 +0800

    Update v100_test.sh

--- experiments/v100_test.sh
+++ experiments/v100_test.sh
@@ -4,7 +4,7 @@ python -u test_multipose.py  \
         --list_start 0 \
         --list_end  10 \
         --dataset_mode single \
-        --gpu_ids 0,1,2,3,4,5,6,7 \
+        --gpu_ids 0,1 \
         --netG rotatespade \
         --norm_G spectralsyncbatch \
         --batchSize 18 \
